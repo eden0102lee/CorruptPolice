56dca1abd033c94ef10e089dd798de1e3c16eaba
eden0102lee <eden0102lee@gmail.com>
2025-06-10 14:57:30 +0800

    Add PlayerUI script

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerUI : MonoBehaviour
+{
+    public Image tokenImage;
+    public TMP_Text numberText;
+
+    public void SetColor(Color color)
+    {
+        if (tokenImage != null)
+            tokenImage.color = color;
+    }
+
+    public void SetPlayerNumber(int number)
+    {
+        if (numberText != null)
+            numberText.text = number.ToString();
+    }
+}
